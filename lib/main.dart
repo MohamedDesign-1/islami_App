937c3fb2929be65d0e0e7341aac988309aaa2d06
m7med <m7medyt2017@gmail.com>
2024-07-06 17:48:51 +0300

    finshind SplashScreen . start in home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:islamiapp/home/homescreen.dart';
+import 'package:islamiapp/pages/home/homescreen.dart';
+import 'package:islamiapp/style/my_theme_app.dart';
 
 void main() {
   runApp(IslamiApp());
@@ -12,6 +13,7 @@ class IslamiApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: MyThemeApp.lightTheme,
         initialRoute: Homescreen.routeName,
         routes: {
           Homescreen.routeName: (context) => Homescreen(),
